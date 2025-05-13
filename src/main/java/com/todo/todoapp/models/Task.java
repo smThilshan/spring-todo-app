4d48175dcc1580a6ebce580fb4b0bd7bd579d2cd
smThilshan <smthilshan@gmail.com>
2025-05-13 19:29:07 +0530

    initial commit

---
+++ src/main/java/com/todo/todoapp/models/Task.java
@@ -0,0 +1,15 @@
+package com.todo.todoapp.models;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
+
+}
