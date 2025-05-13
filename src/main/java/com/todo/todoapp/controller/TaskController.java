f5d75b44439dba90aeda662c98a491bbc230973f
smThilshan <smthilshan@gmail.com>
2025-05-13 22:20:40 +0530

    get mapped

---
+++ src/main/java/com/todo/todoapp/controller/TaskController.java
@@ -0,0 +1,25 @@
+package com.todo.todoapp.controller;
+
+import com.todo.todoapp.models.Task;
+import com.todo.todoapp.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return "tasks";
+    }
+}
---
+++ src/main/java/com/todo/todoapp/services/TaskService.java
@@ -0,0 +1,20 @@
+package com.todo.todoapp.services;
+
+import com.todo.todoapp.models.Task;
+import com.todo.todoapp.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
