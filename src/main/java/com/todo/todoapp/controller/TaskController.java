41ed93d4020e6e334f61cd6c63ffd0c7d7c13780
smThilshan <smthilshan@gmail.com>
2025-05-15 11:29:59 +0530

    added delete and toggle

--- src/main/java/com/todo/todoapp/controller/TaskController.java
+++ src/main/java/com/todo/todoapp/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.todo.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,6 +29,18 @@ public class TaskController {
     @PostMapping
     public String createTask(@RequestParam String title){
        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
         return "redirect:/";
     }
 
