93e9e0d1e55dddba7945eb83a14e4ce76e10d847
smThilshan <smthilshan@gmail.com>
2025-05-15 11:19:09 +0530

    added add task

--- src/main/java/com/todo/todoapp/controller/TaskController.java
+++ src/main/java/com/todo/todoapp/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.todo.todoapp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,4 +24,11 @@ public class TaskController {
         model.addAttribute("tasks",tasks);
         return "tasks";
     }
+
+    @PostMapping
+    public String createTask(@RequestParam String title){
+       taskService.createTask(title);
+        return "redirect:/";
+    }
+
 }
